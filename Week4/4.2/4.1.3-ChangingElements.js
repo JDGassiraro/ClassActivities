0e248e910b3561e5bed9ad93619d2ffffa19d123
JDGassiraro <josephgassiraro2021@gmail.com>
2023-07-07 21:39:26 -0500

    assigment drafts from review

--- Week4/4.2/4.1.3-ChangingElements.js
+++ Week4/4.2/4.1.3-ChangingElements.js
@@ -1,11 +1,23 @@
 // print the outer HTML of the navigation bar
+let outNav = document.querySelector("nav").outerHTML;
+console.log(outNav);
 
 // print the inner HTML of the navigation bar
+let inNav = document.querySelector("nav").innerHTML;
+console.log(inNav);
 
 // print the text content of the #class-schedule-list element
+let textContent = document.querySelector("#class-schedule-list").textContent;
+console.log(textContent);
 
 // select the classList for the first class-week, then add the class "week-1"
+let classList = document.querySelector(".class-week:first-child").classList.add("week-1");
+console.log(classList);
 
 // select the img element and add a src attribute
+let srcAtt = document.querySelector("img").add(src= linkForImageHere)
+console.log(srcAtt);
 
 // change the font color of the h1 element
+let newColor = document.querySelector("h1").style["font-color"] = "gray";
+console.log(newColor);
--- Week4/4.2/4.1.4-CreatingElements.js
+++ Week4/4.2/4.1.4-CreatingElements.js
@@ -1,9 +1,20 @@
 // create a new unordered list (ul) element
+let newUL = document.createElement("ul");
+console.log(newUL);
 
 // remove the paragraph element in the nav-bar
+const child = document.querySelector(".nav-bar").querySelector("p");
+let noP = document.querySelector(".nav-bar").removeChild(child);
+console.log(child);
 
 // add your new ul element to the nav-bar
+let addUL = document.querySelector("nav-bar").appendChild("newUL");
 
 // create two new list item (li) elements, and add some text to them
+let li1 = document.createElement("li");
+li1.textContent = "Hi!"
+let li2 = document.createElement("li");
+li2.textContent = "Hi!"
+
 
 // add the li elements to the ul in the nav-bar
